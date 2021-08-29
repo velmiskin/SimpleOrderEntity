f725dca67763859372ea5355e4d697f6a420b45d
Sergey Velmiskin <asdln@ya.ru>
2021-08-29 20:42:37 +0200

    Add dimensions, add volume calculator, add getters

--- app/Entities/Product.php
+++ app/Entities/Product.php
@@ -11,9 +11,53 @@ class Product extends BaseEntity
 
     public function __construct(private string $name,
                                 private float $price,
+                                private float $weight,
+                                private int $height,
+                                private int $length,
+                                private int $width,
                                 UuidInterface $id)
     {
         parent::__construct($id);
+    }
+
+    /**
+     * Return Product weight in kg
+     *
+     * @return float
+     */
+    public function getWeight(): float
+    {
+        return $this->weight;
+    }
+
+    /**
+     * Return Product height in mm
+     *
+     * @return int
+     */
+    public function getHeight(): int
+    {
+        return $this->height;
+    }
+
+    /**
+     * Return Product length in mm
+     *
+     * @return int
+     */
+    public function getLength(): int
+    {
+        return $this->length;
+    }
+
+    /**
+     * Return Product width in mm
+     *
+     * @return int
+     */
+    public function getWidth(): int
+    {
+        return $this->width;
     }
 
     /**
@@ -30,5 +74,15 @@ class Product extends BaseEntity
     public function getName(): string
     {
         return $this->name;
+    }
+
+    /**
+     * Return Product Volume width in m2
+     *
+     * @return float
+     */
+    public function getProductVolume(): float
+    {
+        return $this->height * $this->length * $this->width / 100;
     }
 }
