a1690abfcc1c0218bae2160f96ad3481002699ef
Sergey Velmiskin <asdln@ya.ru>
2021-08-29 12:13:53 +0200

    Add User Entity

---
+++ app/Entities/User.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Entities;
+
+
+use Ramsey\Uuid\UuidInterface;
+
+class User extends BaseEntity
+{
+
+    protected function __construct(private string $name,
+                                   private string $phone,
+                                   private string $email,
+                                   UuidInterface $id)
+    {
+        parent::__construct($id);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+}
